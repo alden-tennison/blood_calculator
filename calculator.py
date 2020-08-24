3f2d90564f21b4823748fcad10861dad0b43f893
Alden Tennison <alden.tennison@duke.edu>
2020-08-24 12:11:07 -0500

    Add HDL analysis functionality

--- calculator.py
+++ calculator.py
@@ -13,11 +13,23 @@ def interface():
 def HDL_driver():
     print("HDL Interface")
     #get input
+    x = get_input()
     #check if HDL is normal
+    check_HDL(x)
     #output
     
 def get_input():
     x = input("Enter HDL number: ")
     x = int(x)
     return x
+    
+def check_HDL(x):
+    if x>= 60:
+        return "HDL is normal."
+    elif 40<=x<60:
+        return "HDL is borderline low."
+    elif x<40:
+        return "HDL is low."
+    else:
+        return "Error please try again."
 interface()
