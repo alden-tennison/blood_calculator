648f0d59d0c3f47fce653965595aef275b7b16a1
Alden Tennison <alden.tennison@duke.edu>
2020-09-02 11:41:42 -0500

    Create test for calcultor and update calculator

---
+++ test_calculator.py
@@ -0,0 +1,9 @@
+import pytest
+n = "normal."
+bl = "borderline low."
+l = "low."
+@pytest.mark.parametrize("number,expected",[(100,n),(59,bl),(38,l)])
+def test_check_HDL_norm(number,expected):
+    from calculator import check_HDL
+    result = check_HDL(number)
+    assert result == expected
