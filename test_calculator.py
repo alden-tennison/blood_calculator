aa6a379052bafabeceaf7b29573aca8884856280
Alden Tennison <alden.tennison@duke.edu>
2020-09-02 12:09:58 -0500

    tried some pep8 formatting

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,13 @@ import pytest
 n = "normal."
 bl = "borderline low."
 l = "low."
-@pytest.mark.parametrize("number,expected",[(100,n),(59,bl),(38,l)])
+@pytest.mark.parametrize("number,expected",[
+    (100,n),
+    (59,bl),
+    (38,l),
+    ])
+
+
 def test_check_HDL_norm(number,expected):
     from calculator import check_HDL
     result = check_HDL(number)
